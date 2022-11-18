49015443db896a576c984bfa1e1850f898907644
hoangnhattan0907 <69751900+hoangnhattan0907@users.noreply.github.com>
2022-11-19 02:24:31 +0700

    app resource

---
+++ app/src/main/java/com/example/myapplication/presentation/viewmodel/SignInViewModel.java
@@ -0,0 +1,56 @@
+package com.example.myapplication.presentation.viewmodel;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.myapplication.data.model.AppResource;
+import com.example.myapplication.data.model.User;
+import com.example.myapplication.data.remote.dto.UserDTO;
+import com.example.myapplication.data.responsitory.AuthenticationRepository;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class SignInViewModel extends ViewModel {
+    private MutableLiveData<AppResource<User>> userResource = new MutableLiveData<>();
+    private AuthenticationRepository repository;
+
+    public SignInViewModel(Context context){
+        repository = new AuthenticationRepository(context);
+    }
+
+    public LiveData<AppResource<User>> getUserResource(){
+        return userResource;
+    }
+
+    public void signIn(String email, String password){
+        userResource.setValue(new AppResource.Loading(null));
+        Call<AppResource<UserDTO>> callSignIn = repository.signIn(email,password);
+        callSignIn.enqueue(new Callback<AppResource<UserDTO>>() {
+            @Override
+            public void onResponse(Call<AppResource<UserDTO>> call, Response<AppResource<UserDTO>> response) {
+                if (response.isSuccessful()){
+                    AppResource<UserDTO> userDTOAppResource = response.body();
+                    UserDTO userDTO = userDTOAppResource.data;
+                    User user = new User(userDTO.getEmail(),
+                                        userDTO.getName(),
+                                        userDTO.getPhone(),
+                                        userDTO.getToken());
+                    userResource.setValue(new AppResource.Success<>(user));
+                }else{
+                    Log.d("BBB", response.errorBody().toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<AppResource<UserDTO>> call, Throwable t) {
+
+            }
+        });
+    }
+}
