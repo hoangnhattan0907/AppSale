ebda6d956b7339ef96fabd2c670556f0f29342dd
hoangnhattan0907 <69751900+hoangnhattan0907@users.noreply.github.com>
2022-11-19 00:43:28 +0700

    userDTO

--- app/src/main/java/com/example/myapplication/data/remote/RetrofitClient.java
+++ app/src/main/java/com/example/myapplication/data/remote/RetrofitClient.java
@@ -15,47 +15,47 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-        private static RetrofitClient instance = null;
-        private Retrofit retrofit;
-        private ApiService apiService;
+    private static RetrofitClient instance = null;
+    private Retrofit retrofit;
+    private ApiService apiService;
 
-        private RetrofitClient(Context context) {
-            retrofit = createRetrofit(context);
-            apiService = retrofit.create(ApiService.class);
+    private RetrofitClient(Context context) {
+        retrofit = createRetrofit(context);
+        apiService = retrofit.create(ApiService.class);
+    }
+
+    public static RetrofitClient getInstance(Context context) {
+        if (instance == null) {
+            instance = new RetrofitClient(context);
         }
+        return instance;
+    }
 
-        public static RetrofitClient getInstance(Context context) {
-            if (instance == null) {
-                instance = new RetrofitClient(context);
-            }
-            return instance;
+    private Retrofit createRetrofit(Context context) {
+        HttpLoggingInterceptor logRequest = new HttpLoggingInterceptor();
+        logRequest.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .addInterceptor(logRequest)
+                .build();
+
+        Gson gson = new GsonBuilder().create();
+
+        return new Retrofit.Builder()
+                .baseUrl(AppConstant.BASE_URL)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+    }
+
+    public ApiService getApiService() {
+        if (apiService != null) {
+            return apiService;
+        } else {
+            return retrofit.create(ApiService.class);
         }
-
-        private Retrofit createRetrofit(Context context) {
-            HttpLoggingInterceptor logRequest = new HttpLoggingInterceptor();
-            logRequest.setLevel(HttpLoggingInterceptor.Level.BODY);
-
-            OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                    .readTimeout(30, TimeUnit.SECONDS)
-                    .writeTimeout(30, TimeUnit.SECONDS)
-                    .connectTimeout(30, TimeUnit.SECONDS)
-                    .addInterceptor(logRequest)
-                    .build();
-
-            Gson gson = new GsonBuilder().create();
-
-            return new Retrofit.Builder()
-                    .baseUrl(AppConstant.BASE_URL)
-                    .client(okHttpClient)
-                    .addConverterFactory(GsonConverterFactory.create(gson))
-                    .build();
-        }
-
-        public ApiService getApiService() {
-            if (apiService != null) {
-                return apiService;
-            } else {
-                return retrofit.create(ApiService.class);
-            }
-        }
+    }
 }
